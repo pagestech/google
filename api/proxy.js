eb2004df5f443fdb05c58f293f0d88115b047f33
pagestech <pagestech4@gmail.com>
2025-03-13 04:20:45 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://google-ten-blue.vercel.app/";//
+  let target = "https://google.com/";//
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
